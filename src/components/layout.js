d46bb5301c9db72ebc019609aeecd5d64fa4dd12
kiannaliesarmiento <ksarmiento2000@gmail.com>
2019-02-28 21:35:35 -0700

    sign in and sign up routes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
-export default class Layout extends Component {
-  render() {
-    return (
-      <div className='Layout'>
-        {/* <h1>ECOM</h1> */}
-        {this.props.children}
-      
-      </div>
-    );
-  }
-}
+import Header from './headernav/header';
+import Navbar from './headernav/navbar';
+
+export default class Home extends Component {
+      render() {
+        return (
+          <div className='layout'>
+           {this.props.children}
+           <Header/>
+           <Navbar/>
+
+          </div>
+        );
+      }
+    }
+
